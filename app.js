30d84f84230f94fa32440f5bc1a9919041e2a3e7
muhammadkashif20 <kashif28@gmail.com>
2024-07-23 12:40:29 +0500

    COMPLETED

--- app.js
+++ app.js
@@ -3,62 +3,78 @@ let PassInp=document.getElementById('Pass-Inp').addEventListener('keyup',()=>{
     let inp=document.getElementById('Pass-Inp')
     let characLength=document.getElementById('cha-length')
     if(inp.value.length>=8){
-
-     characLength.innerHTML=`<i class="fa-solid fa-circle" style="color: lightgreen; "></i>
+        characLength.innerHTML=`<i class="fa-solid fa-circle" style="color: #00CC66;  margin-left: -1rem;"></i>
      <span>At least 8 characters length</span>`
-        characLength.style.backgroundColor='lightgreen'
+        characLength.style.backgroundColor='#D7F5E7'
         characLength.style.borderRadius='5px'
     }
     else{
         characLength.style.background='transparent'
-
+  characLength.innerHTML=`<i class="fa-solid fa-circle" ></i>
+     <span>At least 8 characters length</span>`
     }
 
     let numberLength=document.getElementById('num-length')
     if(/[0-9]/.test(inp.value)){
-  
-        numberLength.innerHTML=`<i class="fa-solid fa-circle" style="background-color: lightgreen; margin-right: 3rem;"></i>
+
+        numberLength.innerHTML=`<i class="fa-solid fa-circle" style="color: #00CC66;  margin-left: -1rem;"></i>
             <span>At least 1 number (0...9)</span>`
-            numberLength.style.backgroundColor='lightgreen'
+            numberLength.style.backgroundColor='#D7F5E7'
             numberLength.style.borderRadius='5px'
 
     }
     else{
         numberLength.style.backgroundColor='transparent'
-
+            numberLength.innerHTML=`<i class="fa-solid fa-circle" ></i>
+            <span>At least 1 number (0...9)</span>`
     }
 
     let lowerCaseLength=document.getElementById('lower-Length')
     if(/[a-z]/.test(inp.value)){
-           lowerCaseLength.style.backgroundColor='lightgreen'
+         lowerCaseLength.innerHTML=`<i class="fa-solid fa-circle" style="color: #00CC66;  margin-left: -1rem;"></i>
+            <span>At least 1 lowercase letter (a...z)</span>`
+           lowerCaseLength.style.backgroundColor='#D7F5E7'
             lowerCaseLength.style.borderRadius='5px'
     }
     else{
         lowerCaseLength.style.backgroundColor='transparent'
-
+     lowerCaseLength.innerHTML=`<i class="fa-solid fa-circle" ></i>
+            <span>At least 1 lowercase letter (a...z)</span>`
     }
     let upperCaseLength=document.getElementById('upper-Length')
     if(/[A-Z]/.test(inp.value)){
-        upperCaseLength.style.backgroundColor='lightgreen'
+        upperCaseLength.innerHTML=`<i class="fa-solid fa-circle" style="color: #00CC66;  margin-left: -1rem;"></i>
+            <span>At least 1 uppercase letter (A...Z)</span>`
+        upperCaseLength.style.backgroundColor='#D7F5E7'
         upperCaseLength.style.borderRadius='5px'
     }
     else{
         upperCaseLength.style.backgroundColor='transparent'
-
+upperCaseLength.innerHTML=`<i class="fa-solid fa-circle"></i>
+            <span>At least 1 uppercase letter (A...Z)</span>`
     }
     let specialCharacterLength=document.getElementById('special-Length')
     if(/[!@#$%^&*(),.?"]/.test(inp.value)){
-        specialCharacterLength.style.backgroundColor='lightgreen'
+        specialCharacterLength.innerHTML=`<i class="fa-solid fa-circle" style="color: #00CC66;  margin-left: -1rem;"></i>
+            <span>At least 1 special symbol (!...$)</span>`
+        specialCharacterLength.style.backgroundColor='#D7F5E7'
         specialCharacterLength.style.borderRadius='5px'
     }
     else{
         specialCharacterLength.style.backgroundColor='transparent'
-
-    }
- 
-
-
-
-
-   
+        specialCharacterLength.innerHTML=`<i class="fa-solid fa-circle"></i>
+            <span>At least 1 special symbol (!...$)</span>`
+    }  
 })
+let inp = document.getElementById('Pass-Inp');
+let hide = document.getElementById('fa-Eye');
+
+hide.addEventListener('click', () => {
+    if (inp.type === 'password') {
+        inp.type = 'text';
+        hide.innerHTML = `<i class="fa-solid fa-eye" id="fa-Eye"></i>`;
+    } else {
+        inp.type = 'password';
+        hide.innerHTML = `<i class="fa-solid fa-eye-slash" id="fa-Eye"></i>`;
+    }
+});
